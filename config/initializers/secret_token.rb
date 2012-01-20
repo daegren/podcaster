c60bed6b2b038ef6b972bbe46c9d0b0a122f27ce
David Mills <david.mills1@sheridanc.on.ca>
2012-01-19 20:53:23 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podcaster::Application.config.secret_token = 'bf52919e7881f1b7b577cdc439d670fd989ec7826f808ea301ff4566c52bbf90963c0759dbc532fb5c1fe8f06657bda5daefbd0334cdecf06d7f5dcae5eb3f16'
