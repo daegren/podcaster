369357d5ed43a09d3fe925e89bb7fa8c1faafa50
David Mills <david.mills1@sheridanc.on.ca>
2012-02-01 23:45:02 -0500

    updated layouts/design

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -7,4 +7,20 @@ class PodcastsController < ApplicationController
   def new
     @podcast = Podcast.new
   end
+  
+  def create
+    @podcast = Podcast.new(params[:podcast])
+    @podcast.image = "/assets/images/pe.png";
+  
+    if @podcast.save
+      redirect_to @podcast, :notice => 'Post was successfully created.'
+    else
+      render :action => "new"
+    end
+  end
+  
+  def show
+    @podcast = Podcast.find(params[:id])
+  end
+  
 end
